a5c22d779db8be8cc8ce8298907561e0fc2f2320
dotPY-hax <67259802+dotPY-hax@users.noreply.github.com>
2021-09-06 12:45:11 +0200

    make paths absolute

--- files.py
+++ files.py
@@ -1,4 +1,7 @@
-wordlist_file = "files/wordList.txt"
-ps_config_file = "files/PSconfig.ini"
-reflect_file = "files/reflect"
-template_file = "files/template"
+from os import path
+
+root = path.dirname(__file__)
+wordlist_file = path.join(root, "files/wordList.txt")
+ps_config_file = path.join(root, "files/PSconfig.ini")
+reflect_file = path.join(root, "files/reflect")
+template_file = path.join(root, "files/template")
