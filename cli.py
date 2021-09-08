791a2f43b4d9b8227ca5f3e178a4b9a7fb4c54cd
dotPY-hax <67259802+dotPY-hax@users.noreply.github.com>
2021-09-08 20:52:44 +0200

    add powershell only option

--- cli.py
+++ cli.py
@@ -6,13 +6,28 @@ import build
 def run():
     banner()
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", metavar="EXE", help="exe file you want to obfuscate", required=True)
+    parser.add_argument("-e", metavar="EXE", help="exe file you want to obfuscate")
+    parser.add_argument("-p", metavar="POWERSHELL", help="powershell file you want to obfuscate")
     parser.add_argument("-o", metavar="OUTPUT", help="file you want to write to", default="/tmp/totally_legitimate_powershell_file.ps1")
     args = parser.parse_args()
 
+    if (args.e and args.p) or (not args.e and not args.p):
+        parser.print_help()
+        parser.exit()
+
+    if args.e:
+        do_exe(args)
+    elif args.p:
+        do_powershell(args)
+
+
+def do_exe(args):
     script = build.build_final_script(args.e)
     build.write_final_file(script, args.o)
 
+def do_powershell(args):
+    script = build.pure_powershell_obfuscation(args.p)
+    build.write_final_file(script, args.o)
 
 def banner():
     print("=" * 25)
